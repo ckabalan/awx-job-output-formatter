a448826cebfbd00887fb24ac837ec0e22c914f41
Caesar Kabalan <caesar.kabalan@gmail.com>
2019-02-21 09:29:31 -0700

    Update README and script improvements
    Updated the README.md to have contents similar to the PowerShell help
    data. Greatly improved script readability via documentation,
    comments, and cleaner flow.

--- Format-AwxJobOutput.ps1
+++ Format-AwxJobOutput.ps1
@@ -25,17 +25,17 @@ Modify the JobOutput file in-place.
 Do not sort events within each task.
 
 .EXAMPLE
-Format-AwsJobOutput job_12345.txt
+Format-AwxJobOutput job_12345.txt
 
 This command re-combines all Ansible tasks from all servers into their respective task heading.
 
 .EXAMPLE
-Format-AwsJobOutput job_12345.txt -SystemName SERVER1
+Format-AwxJobOutput job_12345.txt -SystemName SERVER1
 
 This command additionally limits the output to only events related to SERVER1.
 
 .EXAMPLE
-Format-AwsJobOutput job_12345.txt -SystemName SERVER1 -Inplace
+Format-AwxJobOutput job_12345.txt -SystemName SERVER1 -Inplace
 
 Same as the above but replaces the contents of the original input file with the output.
 
@@ -62,8 +62,22 @@ Function Format-AwxJobOutput {
 		[Switch]
 		$NoSort=$false
 	)
+	# Match the following lines extracting the following:
+	# included: /var/lib/awx/path/file.yml for SERVER1, SERVER2, SERVER4
+	# 		Extracts: SERVER1, SERVER2, SERVER4
+	# included: /var/lib/awx/path/file.yml for SERVER3
+	# 		Extracts: SERVER3
+	# ok: [SERVER1]
+	# 		Extracts: SERVER1
+	# ok: [SERVER2] => {"module_name": {"key1": "value1", "key2": "value2"}, "changed": false}
+	# 		Extracts: SERVER2
+	# SERVER3                 : ok=67   changed=0    unreachable=0    failed=0   
+	# 		Extracts: SERVER3
+	$ServerNameExtractionRegex = '^(?:(?:.+): \[(.+)\](?: => .+)?)?(?:included: (?:.+) for (.+))?(?:([^\s]+)\s+: ok=\d+\s+changed=\d+\s+unreachable=\d+\s+failed=\d+\s+)?$'
+
 	$JobLog = Get-Content $JobOutput
 
+	# Determine where to save the output
 	if ($Inplace) {
 		$OutputFilename = $JobOutput
 	} elseif ($SystemName) {
@@ -73,35 +87,51 @@ Function Format-AwxJobOutput {
 		$Temp = Get-Item $JobOutput
 		$OutputFilename = $Temp.DirectoryName + [IO.Path]::DirectorySeparatorChar + $Temp.Basename + '_FORMATTED' + $Temp.Extension
 	}
+	# Default lines to go to the header unless we can determine otherwise
 	$ParsedData = [ordered]@{'HEADER' = @()}
 	$Section = 'HEADER'
 	ForEach ($Line in $JobLog) {
+		# Skip blank lines since we'll add them ourselves during the output portion.
 		if ($Line.Length -eq 0) { continue }
 		$AddLine = $false
 		$LineServerName = 'UNKNOWN'
-		if ($Line -match '^(?:(?:.+): \[(.+)\](?: => .+)?)?(?:included: (?:.+) for (.+))?(?:([^\s]+)\s+: ok=\d+\s+changed=\d+\s+unreachable=\d+\s+failed=\d+\s+)?$') {
+		if ($Line -match $ServerNameExtractionRegex) {
+			# Remove the first match, which is the whole line that was matched
 			$Matches.Remove(0)
+			# Remove blank entries
 			$LineServerName = ($Matches.Values.Where({ $_ -ne "" }))[0].ToUpper()
 		}
 		if ($SystemName) {
 			if ($LineServerName -ne 'UNKNOWN') {
 				$TempSystemName = $SystemName.ToUpper()
-				Write-Host $LineServerName
 				if ($LineServerName -match $TempSystemName) {
 					$AddLine = $true
 				}
 			} else {
+				# No matched server name, probably header stuff
 				$AddLine = $true
 			}
 		} else {
+			# Not filtering, add all lines
 			$AddLine = $true
 		}
+		# This section creates a dictionary of arrays with the dictionary keys being the section
+		# header and the array being the section line contents (including header).
+		# Example below:
+		# Key: TASK [role-name : Task Name] ***************************************************
+		# Array Contents:
+		#		TASK [role-name : Task Name] ***************************************************
+		#		ok: [SERVER1] => {"backup": "", "changed": false, "msg": ""}
+		#		ok: [SERVER2] => {"backup": "", "changed": false, "msg": ""}
+		#		ok: [SERVER3] => {"backup": "", "changed": false, "msg": ""}		
 		if ($AddLine) {
 			if (
 				($Line -match 'PLAY \[.+\] \*\*\*+') -or
 				($Line -match 'TASK \[.+\] \*\*\*+') -or
 				($Line -match 'PLAY RECAP \*\*\*+')
 			) {
+				# If this is a header line add a new dictionary key and seed the value array
+				# with the header line as well.
 				$Section = $Line
 				if (-not $ParsedData.Contains($Section)) {
 					$ParsedData.Add($Section, @($Line))
@@ -122,25 +152,22 @@ Function Format-AwxJobOutput {
 
 	ForEach ($Section in $ParsedData.Keys) {
 		if ($Section -eq 'HEADER') {
+			# Skip sorting and other fancy stuff for the special header content
 			$Lines = $ParsedData[$Section]
 		} else {
-			# Print the Section Name
+			# Print the Section header
 			$FinalContent += $ParsedData[$Section][0]
+			# Remove the section header from the array
 			$Lines = $ParsedData[$Section] | Select-Object -Skip 1
 			if ($NoSort -eq $false) {
 				# Sort all the remaining lines by server name for consistency
-				if ($Section.Substring(0, 4) -eq 'TASK') {
-					# For TASK sections
-					$Lines = $Lines | Sort-Object @{Expression={
-						# TODO: Refactor to match functionality from SystemName filter
-						if ($_ -match '(?<Status>.+): \[(?<ServerName>.+)\]( => .+)?$') {
-							$Matches.ServerName
-						}
-					}; Ascending=$true}
-				} else {
-					# For PLAY and PLAY RECAP sections 
-					$Lines = $Lines | Sort-Object
-				}
+				# using our fancy Server Name Extraction Regex from above
+				$Lines = $Lines | Sort-Object @{Expression={
+					if ($_ -match $ServerNameExtractionRegex) {
+						$Matches.Remove(0)
+						($Matches.Values.Where({ $_ -ne "" }))[0].ToUpper()
+					}
+				}; Ascending=$true}
 			}
 		}
 		# Print all server entries sorted
@@ -148,5 +175,6 @@ Function Format-AwxJobOutput {
 		# Blank line before the next section
 		$FinalContent += ''
 	}
+	# Write the file
 	$($FinalContent -Join "`n") | Out-File $OutputFilename
 }
